5d32b53cc66ff275be7b1b977079278a3f06ca99
emsg.85@gmail.com <emsg.85@gmail.com>
2021-02-06 14:07:39 +0530

    banner height issue

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -12,7 +12,7 @@ const banner = (props) => {
                 {
                     `
                     
-                    .wdmatchbannerHeight{height: 600px;display: flex; align-items: center; justify-content: center; width:100%; background: url(/yearonebanner.jpg); position: relative; background-repeat: no-repeat; background-position: center; background-size: cover;}
+                    .wdmatchbannerHeight{height: 600px;display: flex; align-items: center; justify-content: center; width:100%; background: url(/yearonebanner.jpg); position: relative;top:-30px;  background-repeat: no-repeat; background-position: center; background-size: cover;}
                     .wdmatchbanner__title{text-shadow: 2px 5px #000;font-size: 37px; line-height: 60px ; font-weight: bold; color: white; text-align: center;text-transform:uppercase}
                     
                     `
