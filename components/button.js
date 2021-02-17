21a818c7ef2c1b0653a77a2c916b1611733e5d9f
sureshem <48747690+sureshem@users.noreply.github.com>
2021-02-17 15:06:29 +0530

    Update button.js

--- components/button.js
+++ components/button.js
@@ -29,7 +29,7 @@ const Button = (props) => {
             <style jsx>
                 {
                     `
-                    .btn{background-color: transparent;transition: 0.5s;max-width:180px;width: 100%; margin: 0 auto;border: 1px solid #b76f7a; padding: 15px; border-radius: 3px; font-size: 14px; color: #b76f7a;}
+                    .btn{background-color: transparent;transition: 0.5s;max-width:180px;width: 100%; margin:20px auto 10px;border: 1px solid #b76f7a; padding: 15px; border-radius: 3px; font-size: 14px; color: #b76f7a;line-height:15px;text-align:center;}
                     .btn--bgcolor{background-color: #b76f7a;margin: 20px auto 10px;color: #fff;}
                     `
                 }
